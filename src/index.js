add52eb29861c1c55875e66c3bfae173c9885606
Mahadev R Saravad <mahadevrsaravad@gmail.com>
2023-08-26 10:54:48 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./Components/App";
 import './index.css';
 
+ReactDOM.render(<App />,document.getElementById('root'));
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+  
+//     <App />
+ 
+// );
